b56a0764b73dd41f67506601cffdedc085902340
Cesar Rodriguez <streamlink777@live.com>
2020-06-03 15:02:15 -0400

    looping inside react items

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -3,30 +3,29 @@ import Contacts from '../contacts'
 import Card from './Card'
 
 
+function createCard(Contact) {
+return <Card 
+key={Contact.id}
+name={ Contact.name } 
+img={ Contact.imgURL } 
+tel={ Contact.phone } 
+email={ Contact.email } />
+}
+
+
+
 function App() {
-  return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-     
-  <Card
-    name={Contacts[0].name}
-    img={Contacts[0].imgURL}
-    tel={Contacts[0].phone} 
-    email={Contacts[0].email} />
+    return <div>
 
-<Card
-    name={Contacts[1].name}
-    img={Contacts[1].imgURL}
-    tel={Contacts[1].phone} 
-    email={Contacts[1].email} />
+                <h1 className="heading"> My Contacts </h1> 
+                <div Id="contactsList"> 
+                    {Contacts.map(createCard)}
+                </div>
+          </div>
+               
+      
 
-<Card
-    name={Contacts[2].name}
-    img={Contacts[2].imgURL}
-    tel={Contacts[2].phone} 
-    email={Contacts[2].email} />
-    </div>
-  );
+           
 }
 
 export default App;
